93892269eef059f8ac582a3b2d88df3322b87252
Hamza Khan <hamza.khan@technado.pk>
2021-12-24 05:30:06 +0500

    gallery-project

---
+++ includes/footer.php
@@ -0,0 +1,94 @@
+<footer class="foot-sec-1">
+	<div class="row">
+		<div class="col-12 col-md-12 col-lg-12">
+			<div class="footer-image-wrap">
+				<img src="assets/images/foot-cloud.jpg" class="img-fluid">
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="footer-row">
+			<div class="footer-col type-1">
+				<div class="logo-box">
+					<a href="index.php"><img src="assets/images/foot-logo.png" class="img-fluid"></a>
+				</div>
+				<div class="foot-text-wrap">
+					<p class="foot-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+				</div>
+				<ul class="footer-social-icons list-unstyled">
+					<li><a href="#!"><i class="fab fa-facebook-f"></i></a></li>
+					<li><a href="#!"><i class="fab fa-instagram"></i></a></li>
+				</ul>
+			</div>
+			<div class="footer-col type-2">
+				<h1 class="top-heading foot-head-text">Quick Links</h1>
+				<div class="content-wrap-1">
+					<ul class="list-unstyled">
+						<li><a href="index.php"> Home</a></li>
+						<li><a href="#!"> About Us</a></li>
+						<li><a href="mens.php"> Men's</a></li>
+						<li><a href="#!">Women's</a></li>
+						<li><a href="#!">Latest Arrival's</a></li>
+					</ul>
+					<ul class="list-unstyled">
+						<li><a href="#!">Youth</a></li>
+						<li><a href="#!">Hats</a></li>
+						<li><a href="#!">Accessories</a></li>
+						<li><a href="#!">Contact Us</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="footer-col type-3">
+				<h1 class="top-heading type-text-3 foot-head-text">Support</h1>
+				<div class="content-wrap-1">
+					<ul class="list-unstyled">
+						<li><a href="#!">Privacy Policy</a></li>
+						<li><a href="#!"> Terms and conditions</a></li>
+						<li><a href="#!">Policy About</a></li>
+						<li><a href="#!"> Look up my Order</a></li>
+						<li><a href="#!">Faq's</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="footer-col type-3">
+				<h1 class="top-heading type-text-4 foot-head-text">Contact Us</h1>
+				<div class="content-wrap-3">
+					<ul class="list-unstyled">
+						<li><span class="foot-text1">Address:</span><a href="#!"> 1429 Netus Rd, NY 48247</a></li>
+						<li><span class="foot-text1">Email:</span><a href="mailto:example123@gmail.com"> example123@gmail.com</a></li>
+						<li><span class="foot-text1">Phone:</span><a href="tel:123-456-789"> 123-456-789</a></li>
+					</ul>
+					<div class="payment-wrap">
+						<p class="payment-text">Payment Accepted</p>
+						<ul class="payment-list">
+							<li><a href="#!"><img src="assets/images/visa.png" class="img-fluid"></a></li>
+							<li><a href="#!"><img src="assets/images/mastter.png" class="img-fluid"></a></li>
+							<li><a href="#!"><img src="assets/images/master-2.png" class="img-fluid"></a></li>
+							<li><a href="#!"><img src="assets/images/paypal.png" class="img-fluid"></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+<div class="footers-wraps">
+	<div class="container"> 
+		<div class="copyright-bar">	
+			<div class="text-box">	
+				<p>©Copyright 2021 AWTB Apparel. All Right Reserved</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<script src="assets/js/bootstrap.min.js"></script> 
+<script src="assets/js/jquery-3.5.1.min.js"></script>
+<script src="assets/js/stellarnav.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/jquery.fancybox.min.js"></script> 
+<script src="assets/js/custom.js"></script> 
+
+</body>
+</html>
